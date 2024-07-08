d4ff40ac6218f96b38eff4c7463a8b91f74dc14a
William Mosquera <wjmmk3@gmail.com>
2024-07-08 10:18:41 -0500

    product-service implemented

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Main');
   const reset = '\x1b[33m'; //Pone en color tierra el N° del Puerto donde la aplicacion Arranca.
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: envs.port }
+    });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,7 +22,7 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(envs.port);
-  logger.log(`App running on port: ${reset}${envs.port}`)
+  await app.listen();
+  logger.log(`Products MicroService running on port: ${reset}${envs.port}`)
 }
 bootstrap();
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,6 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsNumber, IsPositive } from 'class-validator';
+import { Type } from 'class-transformer';
 
 // Esta linea convierte en Opcional todas las propiedades del CreateProductDto.
  // Para que puedan ser manipuladas en la actualizacion de un registro a DB.
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+    
+    @IsNumber()
+    @IsPositive()
+    @Type( () => Number )
+    public id: number;
+    
+}
